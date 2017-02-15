e56d3658a6cd6d2cfb615eb6203f9194f71b4788
David Burns <davidburns@iinet.net.au>
2017-02-15 15:11:13 +1000

    Combines the story controllers into a single controller

--- src/AppBundle/Controller/StoryController.php
+++ src/AppBundle/Controller/StoryController.php
@@ -7,15 +7,46 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class StoryController extends Controller
 {
+    /**
+     * @Route("/e/{id}", name="edit_story")
+     * @ParamConverter("story", class="AppBundle:Story")
+     * @Template("default/index.html.twig")
+     */
+    public function editAction(Story $story = null, $id, Request $request)
+    {
+        if (!$story) {
+            throw $this->createNotFoundException(
+                'No story found for id '.$id
+            // TODO: Change this error message
+            );
+        }
+
+        // TODO: Check if the story is still active before allowing editing
+
+        if ($request->request->has('next_word')) {
+            $input = $request->request->all();
+            $story->addWord($input['next_word']);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($story);
+            $em->flush();
+        }
+
+        $body = $story->getBody();
+
+        return array('body' => $body);
+    }
+
     /**
      * @Route("/s/{id}", name="story")
      * @ParamConverter("story", class="AppBundle:Story")
      * @Template("Story/story.html.twig")
      */
-    public function loginAction(Story $story = null, $id)
+    public function viewAction(Story $story = null, $id)
     {
         if (!$story) {
             throw $this->createNotFoundException(
@@ -28,4 +59,29 @@ class StoryController extends Controller
         //return $this->render('Story/story.html.twig', array('body' => $body));
         return array('body' => $body);
     }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/new", name="new_story")
+     */
+    public function newAction()
+    {
+        $story = new Story();
+
+        $story->setUser(array($this->getUser()));
+        $story->setBody("");
+        $story->setUrlID(base64_encode(random_bytes(6)));
+        $dt = new \DateTime('now');
+        $story->setCreationDate($dt);
+        $story->setCompletionDate(new \DateTime('now'));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($story);
+        $em->flush();
+
+        //redirect to the edit page
+        return $this->redirectToRoute('edit_story', array('id' => $story->getId()));
+    }
 }
+
+
